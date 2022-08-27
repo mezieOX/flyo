e4d5bfbca3d9aad8e5db6ad841812d56ab6420a2
Ikemma3082 <80706383+Ikemma3082@users.noreply.github.com>
2022-08-27 07:44:06 -0700

    initial commit

--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,9 @@
-import React, { Fragment } from 'react';
-import Layout from '../components/layout/Layout';
-import Trusted from '../components/trusted/Trusted';
-import Info from '../components/info/Info';
-import BusinessTask from '../components/business-task/BusinessTask';
-import LargeCard from '../components/large-card/LargeCard';
-
-
+import React from 'react'
 
 function Home() {
   return (
-    <Fragment>
-    <Layout title="Home">
-      <Info />
-       <Trusted />
-       <BusinessTask />
-      <LargeCard />
-      {/* <GetStarted /> */}
-      {/* <Motive />
-      <Cards />  */}
-    </Layout>
-  </Fragment>
-  );
+    <div>Home</div>
+  )
 }
 
-export default Home;
+export default Home
